70e36139825a55d0c9ef64ae17ba2a3fef102f10
jdclark905 <76894714+jdclark905@users.noreply.github.com>
2021-09-15 10:58:13 -0700

    Completed assignment6

--- AngularPractice/assignment6/src/app/app.component.ts
+++ AngularPractice/assignment6/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultSubscription = 'advanced';
+  user = {
+    email: '',
+    subscriptionType: '',
+    password: ''
+  };
 
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.value.email;
+    this.user.subscriptionType = this.signupForm.value.subscription;
+    this.user.password = this.signupForm.value.password;
+    console.log(this.user.email);
+    console.log(this.user.subscriptionType);
+    console.log(this.user.password);
+  }
 }
